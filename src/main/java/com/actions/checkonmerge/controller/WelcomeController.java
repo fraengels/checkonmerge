efb794698a2778ba863d51a083e0da2aac400ace
Anton Kurbatov <anton.kurbatov@mattilsynet.no>
2023-02-03 01:53:45 +0100

    refactoring

--- src/main/java/com/actions/checkonmerge/controller/WelcomeController.java
+++ src/main/java/com/actions/checkonmerge/controller/WelcomeController.java
@@ -8,6 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WelcomeController {
 
+  @GetMapping
+  public String index() {
+    return "Congratulations! Your application is running!";
+  }
+
   @GetMapping("/welcome")
   public Greeting welcome(@RequestParam String name) {
     return new Greeting(name, "Welcome to this service!");
